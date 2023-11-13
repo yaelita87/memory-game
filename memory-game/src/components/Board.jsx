4738b8b91c95a4c5eb5c5e5c88b927cf1d68f723
Pamela Herrera <pamelayaelherrera@hotmail.com>
2023-11-13 20:37:18 -0300

    ok2

--- memory-game/src/components/Board.jsx
+++ memory-game/src/components/Board.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export const Board = () => {
     return(
-        <div>Board</div>
+        <div className='relative h-screen flex items center'>
+            <h1 className="font-bold text-4xl">Memory game</h1>
+        </div>
     )
 }
