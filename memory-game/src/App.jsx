3994bbd990418b657932c0d6c08b9235f033579a
Pamela Herrera <pamelayaelherrera@hotmail.com>
2023-11-13 20:07:00 -0300

    changes App

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
+import {Board} from "../src/components/Board";
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='App'>
-
+     <Board/>
     </div>
   )
 }
