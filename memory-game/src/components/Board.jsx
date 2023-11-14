346f63933386d28f9d5c8e6ea3080b0709847acf
Pamela Herrera <pamelayaelherrera@hotmail.com>
2023-11-14 19:57:03 -0300

    Board states

--- memory-game/src/components/Board.jsx
+++ memory-game/src/components/Board.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Board = () => {
+    const [cards, setCards] = useState([]);
+    const [flippedCards, setFlippedCards] = useState([]);
+    const [moves, setMoves] = useState(0);
+    const [gameOver, setGameOver] = useState(false);
+    const [isDisabled, setIsDisabled] = useState(false);
+
     return(
         <div className='relative h-screen flex items-center'>
             <h1 className="font-bold text-4xl">Memory game</h1>
