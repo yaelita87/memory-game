1852af0c2204846922bc6c92cb21c290910ce7da
Pamela Herrera <pamelayaelherrera@hotmail.com>
2023-11-14 20:55:40 -0300

    javascript new

--- memory-game/src/components/Board.jsx
+++ memory-game/src/components/Board.jsx
@@ -1,4 +1,13 @@
 import React, { useState } from 'react';
+import {imgs} from '../data';
+
+const shuffleArray = array => {
+    for (let i = array.length -1; i > 0; i--){
+        const j = Math.floor(Math.random() *(i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
 export const Board = () => {
     const [cards, setCards] = useState([]);
@@ -6,6 +15,19 @@ export const Board = () => {
     const [moves, setMoves] = useState(0);
     const [gameOver, setGameOver] = useState(false);
     const [isDisabled, setIsDisabled] = useState(false);
+
+    const createBoard = () => {
+        const duplicatecards = imgs.flatMap((img, i) => {
+            const duplicate = {
+                ...img,
+                id: img.id + imgs.length,
+            };
+            return [img, duplicate];
+        });
+        
+        
+
+    }
 
     return(
         <div className='relative h-screen flex items-center'>
