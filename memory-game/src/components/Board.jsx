f523bed8e9d9ed18634900956967d9a38831b127
Pamela Herrera <pamelayaelherrera@hotmail.com>
2023-11-14 19:46:08 -0300

    tailwind its ok

--- memory-game/src/components/Board.jsx
+++ memory-game/src/components/Board.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Board = () => {
     return(
-        <div className='relative h-screen flex items center'>
+        <div className='relative h-screen flex items-center'>
             <h1 className="font-bold text-4xl">Memory game</h1>
         </div>
     )
