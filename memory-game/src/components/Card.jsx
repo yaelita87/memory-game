3e4dd4014624a2bf84c873b7607ec72c1b160ff1
Pamela Herrera <pamelayaelherrera@hotmail.com>
2023-11-23 22:12:17 -0300

    card js

--- memory-game/src/components/Card.jsx
+++ memory-game/src/components/Card.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 export const Card = ({card}) => {
-    return <div className={`drop-shadow-md flex items-center justify-center cursor-pointer h-16 w-16 hover:scale-105 rounded-x1 transition-all duration-1000`}>
-        <div></div>
-        </div>;
+
+    return ( 
+    <div
+     className={`drop-shadow-md flex items-center
+    ${card.flipped ? '[transform: rotateY(10deg)]' : 'bg-white'} justify-center cursor-pointer h-16 w-16 hover:scale-105 rounded-x1 transition-all duration-1000`}
+    >
+        <div>
+          <img 
+          src={card.img} 
+          alt={card.alt}
+          className={`h-16 scale-110`} 
+          />
+        </div>
+    </div>
+    );
 }
