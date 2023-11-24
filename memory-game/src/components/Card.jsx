b75e70c9b6623773acd4b205a279d168b13a67ff
Pamela Herrera <pamelayaelherrera@hotmail.com>
2023-11-23 22:00:31 -0300

    card styles

--- memory-game/src/components/Card.jsx
+++ memory-game/src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export const Card = () => {
-    return(
-        <div>Card</div>
-    )
+export const Card = ({card}) => {
+    return <div className={`drop-shadow-md flex items-center justify-center cursor-pointer h-16 w-16 hover:scale-105 rounded-x1 transition-all duration-1000`}>
+        <div></div>
+        </div>;
 }
