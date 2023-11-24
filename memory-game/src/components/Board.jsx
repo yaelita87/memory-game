e4ea1e92918a5a98aaa37ebbfac383534e386e87
Pamela Herrera <pamelayaelherrera@hotmail.com>
2023-11-23 21:49:45 -0300

    card panel board

--- memory-game/src/components/Board.jsx
+++ memory-game/src/components/Board.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {imgs} from '../data';
+import {Card} from './Card'
 
 const shuffleArray = array => {
     for (let i = array.length -1; i > 0; i--){
@@ -34,11 +35,23 @@ export const Board = () => {
             }
         })
         setCards(cards)
-    }
+    };
+
+    useEffect(()=> {
+        createBoard();
+    },[]);
 
     return(
         <div className='relative h-screen flex items-center'>
-            <h1 className="font-bold text-4xl">Memory game</h1>
+            <h1 className="font-bold text-4xl">Memory Game</h1>
+        <div className="grid grid-cols-4 gap-3 justify-center items-center px-3 py-5 my-3">
+            {
+                cards.map(card => (
+                    <Card
+                       card={card}
+                       key={card.id}/>
+                ))}
         </div>
-    )
-}
+        </div>
+    );
+};
